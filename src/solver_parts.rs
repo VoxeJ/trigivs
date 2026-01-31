9cdb09c4423de2506e361abc4b21b17f5b98d3b1
Михаил Горин <voxelj@gmail.com>
2026-02-01 00:08:12 +0300

    Added Ruiz scaling and optimized Kaczmarz iterative method

--- src/solver_parts.rs
+++ src/solver_parts.rs
@@ -194,63 +194,140 @@ pub fn solve_givens_sc_rhs_body<T: Float>(sins_cosins: &[(T, T)], r_buffer: &mut
 }
 
 pub fn kaczmarz_body<T: Float>(
-    sub: &[T], 
-    diag: &[T], 
-    sup: &[T], 
-    rhs: &[T], 
-    ai_ai_prod: &[T], 
-    x_buffer: &mut [T], 
-    n: usize, 
-    iter: usize, 
-    eps: T
-) -> Result<(), SolverErrors>{
+    sub: &[T],
+    diag: &[T],
+    sup: &[T],
+    rhs: &[T],
+    ai_ai_prod: &[T],
+    x_buffer: &mut [T],
+    n: usize,
+    iter: usize,
+    eps: T,
+) -> Result<(), SolverErrors> {
     let fade = T::from(0.75).unwrap();
     let min_to_fade = T::from(1.2).unwrap();
     let mut overshoot_counter = T::one();
     let mut prev_r = T::max_value();
-    let mut r = compute_solution_norm(sup, diag, sub, rhs, &x_buffer).unwrap();
-    for _ in 0..iter{
+    let mut r = compute_solution_norm(sup, diag, sub, rhs, x_buffer)?;
+    if n == 1 {
+        if is_zero_eps_mag(diag[0], rhs[0]) {
+            return Err(SolverErrors::DivisionByZero);
+        }
+        x_buffer[0] = rhs[0] / diag[0];
+        return Ok(());
+    }
+    for _ in 0..iter {
         let mut contin = false;
-        for i in 0..n{
-            let xi_ai_dotproduct = 
-            if n == 1{
-                diag[0] * x_buffer[0]
-            } else if i == 0 {
-                diag[0] * x_buffer[0] + sup[0] * x_buffer[1]
-            } else if i == n - 1 {
-                diag[n-1] * x_buffer[n-1] + sub[n - 2] * x_buffer[n-2]
-            } else {
-                sub[i-1] * x_buffer[i - 1] + diag[i] * x_buffer[i] + sup[i] * x_buffer[i+1]
-            };
-            let w = T::one()/(overshoot_counter + if is_zero_eps_mag(prev_r, r) {T::one()} else {r/prev_r});
+        for i in 0..n {
+            let mut xi_ai_dotproduct = diag[i] * x_buffer[i];
+            if i < n - 1{
+                xi_ai_dotproduct = xi_ai_dotproduct + sup[i] * x_buffer[i + 1];
+            }
+            if i > 0 {
+                xi_ai_dotproduct = xi_ai_dotproduct + sub[i - 1] * x_buffer[i - 1];
+            }
+            let w = T::one() / (overshoot_counter + if is_zero_eps_mag(prev_r, r) { T::one() } else { r / prev_r });
             let numerator = (rhs[i] - xi_ai_dotproduct) * w;
-            if is_zero_eps_mag(ai_ai_prod[i], numerator){
+            if is_zero_eps_mag(ai_ai_prod[i], numerator) {
                 return Err(SolverErrors::DivisionByZero);
             }
             let c = numerator / ai_ai_prod[i];
-            if n == 1 {
-                x_buffer[0] = x_buffer[0] + c * diag[0];
-            } else if i == 0{
-                x_buffer[0..=1].iter_mut().zip([diag[0], sup[0]]).for_each(|(element, a)| *element = *element + c * a);
-            } else if i == n - 1{
-                x_buffer[n-2..=n-1].iter_mut().zip([sub[n-2], diag[n-1]]).for_each(|(element, a)| *element = *element + c * a);
-            } else {
-                x_buffer[i-1..=i+1].iter_mut().zip([sub[i-1], diag[i], sup[i]]).for_each(|(element, a)| *element = *element + c * a );
+            x_buffer[i] = x_buffer[i] + c * diag[i];
+            if i < n - 1 {
+                x_buffer[i+1] = x_buffer[i+1] + c * sup[i];
             }
-            if overshoot_counter > min_to_fade{
+            if i > 0{
+                x_buffer[i - 1] = x_buffer[i - 1] + c * sub[i - 1];
+            }
+            if overshoot_counter > min_to_fade {
                 overshoot_counter = overshoot_counter * fade;
             }
-            if prev_r > r{
+            if prev_r > r {
                 overshoot_counter = overshoot_counter + T::one();
             }
             prev_r = r;
-            r = compute_solution_norm(sup, diag, sub, rhs, &x_buffer).unwrap();
-            if eps < r{
+            r = compute_solution_norm(sup, diag, sub, rhs, x_buffer)?;
+            if r > eps {
                 contin = true;
             }
         }
-        if contin == false{
+        if !contin {
             break;
+        }
+    }
+    Ok(())
+}
+
+pub fn ruiz_equilibrium_body<T: Float>(
+    d_buffer: &mut [T],
+    sup_buffer: &mut [T], 
+    sub_buffer: &mut [T],
+    row_buffer: &mut [T],
+    col_buffer: &mut [T],
+    iter: usize,
+    eps: T
+) -> Result<(), SolverErrors>{
+    let n = d_buffer.len();
+    if n == 1 {
+        if d_buffer[0].abs() < T::epsilon() {
+            return Err(SolverErrors::DivisionByZero);
+        }
+        row_buffer[0] = T::one() / d_buffer[0].abs();
+        d_buffer[0] = T::one().copysign(d_buffer[0]);
+        return Ok(());
+    }
+    for _ in 0..iter{
+        let mut max_norm_diff = T::zero();
+        for i in 0..n {
+            let mut div = (d_buffer[i]).abs() * col_buffer[i];
+            if i < n - 1{
+                div = div.max(sup_buffer[i].abs() * col_buffer[i + 1]);
+            }
+            if i > 0 {
+                div = div.max(sub_buffer[i - 1].abs() * col_buffer[i - 1]);
+            }
+            div = div.sqrt();
+
+            if div < T::epsilon() {
+                return Err(SolverErrors::DivisionByZero);
+            }
+
+            let r = T::one() / div;
+
+            row_buffer[i] = row_buffer[i] * r;
+        }
+        
+        for i in 0..n {
+            let mut div = d_buffer[i].abs() * row_buffer[i];
+            if i < n - 1{
+                div = div.max(sub_buffer[i].abs() * row_buffer[i + 1]);
+            }
+            if i > 0 {
+                div = div.max(sup_buffer[i - 1].abs() * row_buffer[i - 1]);
+            }
+            div = div.sqrt();
+            if div < T::epsilon() {
+                return Err(SolverErrors::DivisionByZero);
+            }
+            let c = T::one() / div;
+            col_buffer[i] = col_buffer[i] * c;
+
+            let curr_norm_diff = (div - T::one()).abs();
+            if max_norm_diff < curr_norm_diff{
+                max_norm_diff = curr_norm_diff;
+            }
+        }
+        if max_norm_diff < eps{
+            break;
+        }
+    }
+    for i in 0..n{
+        d_buffer[i] = d_buffer[i] * row_buffer[i] * col_buffer[i];
+        if i < n - 1 {
+            sup_buffer[i] = sup_buffer[i] * row_buffer[i] * col_buffer[i + 1];
+        }
+        if i > 0 {
+            sub_buffer[i-1] = sub_buffer[i-1] * row_buffer[i] * col_buffer[i - 1]; 
         }
     }
     Ok(())
